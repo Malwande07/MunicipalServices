6c9b1d9cdb65e90a72854e08304227a6114899eb
malwa <st10280981@vcconnect.edu.za>
2025-09-10 16:31:24 +0200

    added comments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,3 +30,15 @@ namespace MunicipalServices.Controllers
         }
     }
 }
+
+/*
+• Bootstrap. (2025) Bootstrap Documentation. Available at: https://getbootstrap.com/ (Accessed: 10 September 2025).
+• File Upload in ASP.NET Core. (2025) Microsoft Docs – Upload files in ASP.NET Core. Available at: https://learn.microsoft.com/aspnet/core/mvc/models/file-uploads (Accessed: 10 September 2025).
+• FontAwesome. (2025) FontAwesome Icons. Available at: https://fontawesome.com/ (Accessed: 10 September 2025).
+• GitHub. (2025) GitHub – Code Hosting and Collaboration Platform. Available at: https://github.com/ (Accessed: 10 September 2025).
+• HTML & CSS Tutorials. (2025) MDN Web Docs – HTML and CSS. Available at: https://developer.mozilla.org/en-US/docs/Web (Accessed: 10 September 2025).
+• jQuery. (2025) jQuery API Documentation. Available at: https://api.jquery.com/ (Accessed: 10 September 2025).
+• Razor Pages Documentation. (2025) Razor Pages in ASP.NET Core. Available at: https://learn.microsoft.com/aspnet/core/razor-pages/ (Accessed: 10 September 2025).
+• Stack Overflow. (2025) Stack Overflow – Developer Community. Available at: https://stackoverflow.com/ (Accessed: 10 September 2025).
+• W3Schools. (2025) W3Schools Online Web Tutorials. Available at: https://www.w3schools.com/ (Accessed: 10 September 2025).
+*/
